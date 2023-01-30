081a1a38d03013366611cb4d479ad6b6a9dfd5dc
송성빈 <aktm1004@naver.com>
2023-01-30 22:21:00 +0900

    [Bronze V] Title: Hello World, Time: 112 ms, Memory: 13828 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,6 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        char[] HelloWord = {72, 101, 108, 108, 111, ' ', 87,  111, 114, 108, 100, 33};
-        System.out.println(HelloWord);
+interface Main{
+    static void main(String[]A){
+        System.out.print("Hello World!");
     }
 }
