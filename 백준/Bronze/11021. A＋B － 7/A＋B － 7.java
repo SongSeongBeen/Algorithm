e53a4b2d78045299a13d22b864709cea9528acb9
송성빈 <aktm1004@naver.com>
2023-02-08 17:58:54 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,14 +1,8 @@
-	import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*; 
 public class Main {
-	public static void main(String args[]) throws IOException {
- 
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
+	public static void main(String args[]) throws IOException { 
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));		
 		int a = Integer.parseInt(br.readLine());
- 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= a; i++) {
 			String str = br.readLine();
