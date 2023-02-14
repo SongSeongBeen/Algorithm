80d9f1df4f20669455e046e6b4ea3a2e75e074b6
송성빈 <aktm1004@naver.com>
2023-02-14 10:31:54 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,1 @@
-	import java.util.Scanner;
-public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);	
-		int a = in.nextInt();
-		
-		in.close();		
-		
-		for(int i = 1; i<10;i++) {
-			System.out.println(a+" * "+i+" = "+(a*i));
-		}
-	}
-}
+import java.util.*;class Main {public static void main(String[] args) {	for(int s = new Scanner(System.in).nextInt(), i=1;i<10;i++) {System.out.println(s+" * "+i+" = "+(s*i));}}}
