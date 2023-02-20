cc491c76c3acdd5e2660917748bc18048ce0afeb
송성빈 <aktm1004@naver.com>
2023-02-20 13:29:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        char a = new Scanner(System.in).next().charAt(0);
+        System.out.print((int)a);
+    }
+}
