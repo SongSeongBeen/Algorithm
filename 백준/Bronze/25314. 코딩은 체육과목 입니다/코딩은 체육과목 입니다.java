34089f3c36c6b00683357cf582f49cadd83f2f64
송성빈 <aktm1004@naver.com>
2023-02-21 16:34:00 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,9 +1,11 @@
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()) / 4;
-        System.out.print("long ".repeat(n) + "int");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine())/4;
+        System.out.print("long ".repeat(n)+"int");
+        
     }
 }
