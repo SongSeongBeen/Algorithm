535548c5052ea81ed30c920c7db36f791e2bc8c9
송성빈 <aktm1004@naver.com>
2023-02-21 16:08:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        long a = s.nextLong(), b = s.nextLong(), c = s.nextLong();
+        System.out.print(a+b+c);
+    }
+}
