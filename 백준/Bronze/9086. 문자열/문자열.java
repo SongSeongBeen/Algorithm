3f35619087581a8f129ae882f9472aa7b10e6a7e
송성빈 <aktm1004@naver.com>
2023-02-22 16:57:15 +0900

    [Bronze V] Title: 문자열, Time: 212 ms, Memory: 17508 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		int n = sc.nextInt();
+		String str[]= new String[n];
+		for(int i=0; i<n; i++) {
+			str[i] = sc.next();
+		}
+		for(int i=0; i<n; i++) {
+			System.out.println(String.valueOf(str[i].charAt(0)) +String.valueOf(str[i].charAt(str[i].length()-1)));
+		}		
+	}
+}
