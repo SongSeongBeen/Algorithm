75d4ea8e2379f113434ab696bc80d1aeedc52884
송성빈 <aktm1004@naver.com>
2023-02-23 09:47:33 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 228 ms, Memory: 18368 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Main{
    	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);	
 		
 		int[] arr = new int[26];
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = -1;
-		}
+		Arrays.fill(arr, -1);
 						
 		String str = sc.nextLine();
 		
