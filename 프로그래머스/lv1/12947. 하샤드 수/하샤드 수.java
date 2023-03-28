0fe8c13cbd41d0b9d6f7116caacc32fa7e2f43fb
송성빈 <aktm1004@naver.com>
2023-03-28 14:43:43 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int temp = x;
+        int sum = 0;
+        
+        while(temp >= 10){
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        sum += temp;
+        
+        return x % sum ==0;
+            
+    }
+}
