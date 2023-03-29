69a2c2beac929e7d1d20dfaa85eb6aca57d566f0
송성빈 <aktm1004@naver.com>
2023-03-30 00:33:33 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 1.13 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+class Solution {
+    public int solution(int balls, int share) {
+        BigInteger n = calculateFactorial(balls);
+        BigInteger m = calculateFactorial(balls - share).multiply(calculateFactorial(share));
+        return n.divide(m).intValue();
+    }
+    
+    private static BigInteger calculateFactorial(int n) {
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        return factorial;
+    }
+}
