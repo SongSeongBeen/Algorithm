1c53121d3f64da36c1250d1528f79d6de41a281a
송성빈 <aktm1004@naver.com>
2023-04-03 20:08:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,16 +1,11 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
-            for(String[] user : db){
-                System.out.println(id_pw[0].equals(user[0]) && id_pw[1].equals(user[1]));
-                if(id_pw[0].equals(user[0]) && id_pw[1].equals(user[1])){
-                    answer ="login";
-                } else if(id_pw[0].equals(user[0]) && !id_pw[1].equals(user[1])) {
-                    answer = "wrong pw";
-                } else if(!id_pw[0].equals(user[0]) && !id_pw[1].equals(user[1])) {
-                    answer = "fail";
-                }
+        for(String[] user : db) {
+            if(id_pw[0].equals(user[0])) {
+                return id_pw[1].equals(user[1]) ? "login" : "wrong pw";
             }
-        return answer;
+        }
+        return "fail";
     }
 }
