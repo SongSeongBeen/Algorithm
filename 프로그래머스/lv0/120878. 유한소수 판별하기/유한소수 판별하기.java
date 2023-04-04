d3817a63c9e668b0130475c8efc052902dca8877
송성빈 <aktm1004@naver.com>
2023-04-04 17:59:37 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,48 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        
-        int gcd = gcd(a, b);
-        a /= gcd;
-        b /= gcd;
-        
-        if (b == 1 || isFinite(b)) {
-            answer = 1;
-        } else {
-            answer = getRecurringCycle(b) > 0 ? 2 : 1;
-        }
-        
-        return answer;
-    }
-    
-    public int gcd(int a, int b) {
-        while (b != 0) {
-            int r = a % b;
-            a = b;
-            b = r;
-        }
-        return a;
-    }
-    
-    public boolean isFinite(int b) {
-        while (b % 2 == 0) {
-            b /= 2;
-        }
-        while (b % 5 == 0) {
-            b /= 5;
-        }
-        return b == 1;
-    }
-    
-    public int getRecurringCycle(int b) {
-        int[] remainders = new int[b];
-        int remainder = 1;
-        int i = 0;
-        while (remainders[remainder] == 0 && remainder != 0) {
-            remainders[remainder] = i;
-            remainder = (remainder * 10) % b;
-            i++;
-        }
-        return remainder == 0 ? 0 : i - remainders[remainder];
+        for (;b%2<1;) b /= 2;
+        for (;b%5<1;) b /= 5;
+        return a%b<1 ? 1 : 2;
     }
 }
