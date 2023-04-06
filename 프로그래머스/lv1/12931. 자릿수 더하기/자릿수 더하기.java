7ada1b0b3d29e7c62726b226c66c12850540f690
송성빈 <aktm1004@naver.com>
2023-04-06 14:39:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,7 +1,12 @@
 public class Solution {
     public int solution(int n) {
-        return String.valueOf(n).chars()
-                                 .map(Character::getNumericValue)
-                                 .sum();
+        int answer = 0;
+        
+        while(n>0){
+            answer += n%10;
+            n/=10;
+        }
+
+        return answer;
     }
 }
