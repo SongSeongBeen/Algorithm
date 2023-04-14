61f8101eac86b7575f01f71fe2548ebb82a27ef5
송성빈 <aktm1004@naver.com>
2023-04-15 08:27:58 +0900

    [Silver IV] Title: 개미, Time: 112 ms, Memory: 12896 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int W =sc.nextInt();
+        int H =sc.nextInt();
+        int P =sc.nextInt();
+        int Q =sc.nextInt();
+        int T =sc.nextInt();
+       
+        int currentX = (T + P) % (2 * W);
+        int currentY = (T + Q) % (2 * H);
+        if(currentX > W) currentX = 2 * W - currentX;
+        if(currentY > H) currentY = 2 * H - currentY;
+        System.out.print(currentX + " " + currentY);
+    }
+}
