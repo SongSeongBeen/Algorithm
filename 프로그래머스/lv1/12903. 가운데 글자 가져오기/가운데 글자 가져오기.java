f83c1fb27810604c51770f84f88d13d8572daeae
송성빈 <aktm1004@naver.com>
2023-04-28 17:41:15 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,13 +1,6 @@
-import java.util.*;
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        char[] chr = new char[s.length()];
-        for(int i =0; i<s.length(); i++){
-            chr[i] = s.charAt(i);
-        }
-        answer = s.length() % 2 == 0 ? String.valueOf(chr[s.length() / 2 - 1]) + String.valueOf(chr[s.length() / 2]) : String.valueOf(chr[s.length() / 2]);
-
-        return answer;
+        int mid = s.length() / 2;
+        return s.length() % 2 == 0 ? s.substring(mid - 1, mid + 1) : s.substring(mid, mid + 1);
     }
 }
