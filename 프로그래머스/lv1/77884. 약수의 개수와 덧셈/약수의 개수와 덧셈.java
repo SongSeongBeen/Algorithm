001cec13321cfffe0bf6d1b3a77204a2fd056960
송성빈 <aktm1004@naver.com>
2023-05-17 09:32:50 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,21 +1,11 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-      
-        for (int i = left; i <= right; i++) {
-            int divisorCount = countDivisors(i);
-            answer += (divisorCount % 2 == 0) ? i : -i;
+        for (int i = left; i <= right; i++) {  
+            answer += (i % Math.sqrt(i) == 0) ? -i : i;
         }
         
         return answer;
     }
-    
-    private int countDivisors(int num) {
-        int count = 0;
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0)
-                count++;
-        }
-        return count;
-    }
+
 }
