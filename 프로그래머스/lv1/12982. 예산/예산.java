9779dd6d644445959856b04fe03ccb5a0268e5cd
송성빈 <aktm1004@naver.com>
2023-06-05 17:42:47 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        for (int i = 0; i < d.length && budget >= d[i]; i++) {
+            budget -= d[i];
+            answer++;
+        }
+        return answer;
+    }
+}
