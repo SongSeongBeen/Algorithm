c5747a449e03063dd9eecff2bbc0eb3242b4127d
송성빈 <aktm1004@naver.com>
2023-07-24 13:34:07 +0900

    [unrated] Title: 정수 찾기, Time: 1.36 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] num_list, int n) {
-        boolean bolChk = Arrays.stream(num_list).anyMatch(i -> i == n);
-        return bolChk == true ? 1 : 0;
+        return Arrays.stream(num_list).anyMatch(i -> i == n) ?  1 : 0;
     }
 }
