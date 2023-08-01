71fea82f248887708921a48ecc2e7dcbf83cd3d3
송성빈 <aktm1004@naver.com>
2023-08-01 11:30:33 +0900

    [unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -3,24 +3,20 @@ class Solution {
 
         int arr1Sum = 0;
         int arr2Sum = 0;
-        
-        if(arr1.length < arr2.length){
-            return -1;
-        } else if(arr1.length > arr2.length){
-            return 1;
-        } else {
-            for(int i = 0; i < arr1.length; i++){
-                arr1Sum += arr1[i];
-                arr2Sum += arr2[i];
-            }
+        if(arr1.length != arr2.length){
+            return arr1.length < arr2.length ? -1 : 1;
+        } 
 
-            if(arr1Sum == arr2Sum){
-                return 0;
-            } else if(arr1Sum > arr2Sum) {
-                return 1;
-            } else {
-                return -1;
-            }           
-        }  
+        for(int i = 0; i < arr1.length; i++){
+            arr1Sum += arr1[i];
+            arr2Sum += arr2[i];
+        }
+
+        if(arr1Sum != arr2Sum){
+            return arr1Sum > arr2Sum ? 1 : -1;
+        } else {
+            return 0;
+        }          
+        
     }
 }
