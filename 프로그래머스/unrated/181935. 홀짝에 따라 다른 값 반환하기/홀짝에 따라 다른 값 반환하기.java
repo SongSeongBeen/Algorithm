b4507f067c86becf1006054689f1669ebcb6f964
송성빈 <aktm1004@naver.com>
2023-08-02 11:29:27 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -2,15 +2,12 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int n) {
-        return (n % 2 != 0) ? evenNumSum(n) : oddNumSum(n);
-    }
-    
-    private int evenNumSum(int n) {
-        return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).sum();
-    }
+        int answer = 0;
 
-    private int oddNumSum(int n) {
-        return IntStream.rangeClosed(1, n).filter(i -> i % 2 == 0).map(i -> i * i).sum();
-    }
+        for(int i = n; i >= 0; i -= 2) {
+            answer += (n % 2 == 0) ? i * i : i;    
+        }
 
+        return answer;
+    }
 }
