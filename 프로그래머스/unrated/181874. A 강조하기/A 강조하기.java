9b0ee3e2b016313c484ef6b54ae716d81474c619
송성빈 <aktm1004@naver.com>
2023-08-02 09:56:03 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        for(char s : myString.toCharArray()){
-             sb.append((s == 'a' || s == 'A') ? 'A' : Character.toLowerCase(s));
-        }
-        return sb.toString();
+        return myString.toLowerCase().replace("a", "A");
     }
 }
