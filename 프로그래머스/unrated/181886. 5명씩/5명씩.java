021b1822273c2522032e27bf3ddf3f92697cff28
송성빈 <aktm1004@naver.com>
2023-08-04 17:59:35 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,13 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] names) {
-        List<String> result = new ArrayList<>();
-
-        for(int i = 0; i < names.length; i += 5) {
-            result.add(names[i]);
-        }
-
-        return result.toArray(new String[0]);
+        String[] answer = {};
+        return answer;
     }
 }
