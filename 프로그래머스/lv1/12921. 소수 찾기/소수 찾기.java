8304cd30d94de78b3deeaa48f403a31cab332a5e
송성빈 <aktm1004@naver.com>
2023-08-04 17:32:18 +0900

    [level 1] Title: 소수 찾기, Time: 12.01 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,23 +1,29 @@
 class Solution {
-    public int solution(int n) {
-        boolean[] primeArray = new boolean[n + 1]; 
-        int answer = 0;
+    public static int solution(int n) {
+        boolean[] primeArray = checkPrimes(n);
 
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (!primeArray[i]) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private static boolean[] checkPrimes(int n) {
+        boolean[] primeArray = new boolean[n + 1];
         int root = (int) Math.sqrt(n);
 
-        for (int i = 2; i <= root; i++) { 
-            if (!primeArray[i]) { 
-                for (int j = i * i; j <= n; j += i) { 
+        for (int i = 2; i <= root; i++) {
+            if (!primeArray[i]) {
+                for (int j = i * i; j <= n; j += i) {
                     primeArray[j] = true;
                 }
             }
         }
 
-        for (int i = 2; i <= n; i++) { 
-            if (!primeArray[i]) 
-                answer++;
-        }
-
-        return answer;
+        return primeArray;
     }
 }
