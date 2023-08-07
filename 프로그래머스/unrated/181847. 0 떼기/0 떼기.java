c61daaaf3102260aef9c062c8c9126a649bfaca2
송성빈 <aktm1004@naver.com>
2023-08-07 10:26:49 +0900

    [unrated] Title: 0 떼기, Time: 7.26 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,9 +1,5 @@
 public class Solution {
     public String solution(String n_str) {
-        int startIndex = 0;
-        while (startIndex < n_str.length() && n_str.charAt(startIndex) == '0') {
-            startIndex++;
-        }
-        return n_str.substring(startIndex);
+        return ""+Integer.parseInt(n_str);
     }
 }
