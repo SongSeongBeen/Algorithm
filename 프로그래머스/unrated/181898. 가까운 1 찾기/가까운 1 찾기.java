4118408ac1665a4b384bf62e90b1a45adf87f940
송성빈 <aktm1004@naver.com>
2023-08-08 13:13:07 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        for(int i = 0; i < arr.length; i++) {
+            if (idx <= i && arr[i] == 1) return i; 
+        }
+        return -1;
+    }
+}
