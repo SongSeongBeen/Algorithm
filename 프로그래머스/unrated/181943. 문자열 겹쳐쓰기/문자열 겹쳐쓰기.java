c6e63980c1c1c2a83669ce571a75a9aacc3b360d
송성빈 <aktm1004@naver.com>
2023-08-08 13:22:55 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.06 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,5 +1,18 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        return my_string.substring(0,s) + overwrite_string + my_string.substring(overwrite_string.length()+s);
+        if (s >= my_string.length()) {
+            return my_string; 
+        }
+
+        StringBuilder sb = new StringBuilder(my_string);
+        for (int i = 0; i < overwrite_string.length() && s + i < sb.length(); i++) {
+            sb.setCharAt(s + i, overwrite_string.charAt(i));
+        }
+
+        if (s + overwrite_string.length() > sb.length()) {
+            sb.append(overwrite_string.substring(sb.length() - s));
+        }
+
+        return sb.toString();
     }
 }
