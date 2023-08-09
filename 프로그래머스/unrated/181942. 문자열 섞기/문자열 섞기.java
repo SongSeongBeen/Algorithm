3237246ef5c58cebead021765ef3edb5438f8518
송성빈 <aktm1004@naver.com>
2023-08-09 14:10:42 +0900

    [unrated] Title: 문자열 섞기, Time: 0.05 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        int minLength = Math.min(str1.length(), str2.length()); //예외방지
+
+        for(int i = 0; i < minLength; i++){
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
