436e2ba5f66c675dab918ef5ca2d959361553606
송성빈 <aktm1004@naver.com>
2023-08-09 13:28:29 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.37 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,15 +1,39 @@
 class Solution {
-    public int solution(int a, int b, int c) {
-        int intPlus = a + b + c;
-        int intSqrt = (int)(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
-        int intPow = (int)(Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
-        
-        if(a != b && a != c && b != c) {
-            return intPlus;
-        }  else if(a == b && a == c && b == c){
-            return intPlus * intSqrt * intPow;
-        } else {
-            return intPlus * intSqrt;
+
+    private class Numbers {
+        private int a, b, c;
+        private int sum, sqrtSum, powSum;
+
+        private Numbers(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.sum = a + b + c;
+            this.sqrtSum = (int)(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+            this.powSum = (int)(Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
         }
+
+        private boolean allEqual() {
+            return a == b && a == c;
+        }
+
+        private boolean allDifferent() {
+            return a != b && a != c && b != c;
+        }
+
+        private int calculate() {
+            if (allDifferent()) {
+                return sum;
+            } else if (allEqual()) {
+                return sum * sqrtSum * powSum;
+            } else {
+                return sum * sqrtSum;
+            }
+        }
+    }
+
+    public int solution(int a, int b, int c) {
+        Numbers numbers = new Numbers(a, b, c);
+        return numbers.calculate();
     }
 }
