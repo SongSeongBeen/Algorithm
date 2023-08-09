f454064a907cf40bd9833117b84f612d33195775
송성빈 <aktm1004@naver.com>
2023-08-09 10:12:11 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.08 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -5,15 +5,11 @@ class Solution {
         int operand1 = Integer.parseInt(strArrBin[0]);
         int operand2 = Integer.parseInt(strArrBin[2]);
 
-        switch(strArrBin[1]){
-            case "+" : 
-                return operand1 + operand2;
-            case "-" : 
-                return operand1 - operand2;
-            case "*" : 
-                return operand1 * operand2;
-            default:
-                throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
-        }
+        return switch(strArrBin[1]){
+            case "+" -> operand1 + operand2;
+            case "-" -> operand1 - operand2;
+            case "*" -> operand1 * operand2;
+            default -> throw new IllegalArgumentException(strArrBin[1] + "은 지원하지 않는 연산자입니다.");
+        };
     }
 }
