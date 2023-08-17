e669578b041efee090969ca95337c930b4720b11
송성빈 <aktm1004@naver.com>
2023-08-17 15:26:53 +0900

    [unrated] Title: 문자열 뒤집기, Time: 18.52 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        
-        String sub = my_string.substring(s, e + 1);
-        String reversedSub = new StringBuilder(sub).reverse().toString();
-
-        return my_string.substring(0, s) + reversedSub + my_string.substring(e + 1);
+        return my_string.substring(0, s) + new StringBuilder(my_string.substring(s, e + 1)).reverse().toString() + my_string.substring(e + 1);
     }
 }
