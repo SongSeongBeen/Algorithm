a32fb1415df1d59303bfd539bb309e641ad7283d
송성빈 <aktm1004@naver.com>
2023-09-08 09:23:04 +0900

    [unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -2,10 +2,8 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         for (int num : num_list) {
-            if (num % 2 != 0)  num -= 1;
-            
             while (num > 1) {
-                num /= 2;
+                num = (num % 2 == 0) ? num / 2 : (num - 1) / 2;
                 answer++;
             }
         }
