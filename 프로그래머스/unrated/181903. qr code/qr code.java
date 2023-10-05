e0ab0265bb0309d6ebb4ef54333601ff165ddc7c
송성빈 <aktm1004@naver.com>
2023-10-05 10:24:10 +0900

    [unrated] Title: qr code, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int q, int r, String code) {
-       StringBuilder answer = new StringBuilder();
+       // 최대로 필요한 문자의 개수를 계산하여 StringBuilder의 초기 크기를 설정
+       int initialCapacity = (code.length() - r + q - 1) / q;
+       StringBuilder answer = new StringBuilder(initialCapacity);
        
-       for (int i = 0; i < code.length(); i++) {
-           if (i % q == r) {
-               answer.append(code.charAt(i));
-           }
+       for (int i = r; i < code.length(); i += q) {
+           answer.append(code.charAt(i));
        }
         
         return answer.toString();
