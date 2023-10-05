7b0166aa6eea0f1e0d21a4ecb59dd0cd195484d7
송성빈 <aktm1004@naver.com>
2023-10-05 09:37:08 +0900

    [unrated] Title: 커피 심부름, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -1,12 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
+
+    private static final Map<String, Integer> MapPrice;
+
+    static {
+        MapPrice = new HashMap<>();
+        MapPrice.put("iceamericano", 4500);
+        MapPrice.put("americanoice", 4500);
+        MapPrice.put("hotamericano", 4500);
+        MapPrice.put("americanohot", 4500);
+        MapPrice.put("americano", 4500);
+        MapPrice.put("anything", 4500);
+        MapPrice.put("icecafelatte", 5000);
+        MapPrice.put("cafelatteice", 5000);
+        MapPrice.put("hotcafelatte", 5000);
+        MapPrice.put("cafelattehot", 5000);
+        MapPrice.put("cafelatte", 5000);
+    }
+
     public int solution(String[] order) {
         int answer = 0;
-        for (String item : order) {
-            if (item.equals("anything") || item.contains("americano")) {
-                answer += 4500;
-            } else {
-                answer += 5000;
-            }
+        for (String strOrder : order) {
+            answer += MapPrice.getOrDefault(strOrder, 0);
         }
         return answer;
     }
