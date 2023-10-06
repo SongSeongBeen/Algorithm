306c02c5470238568ee9f1afb7f19101d4611390
송성빈 <aktm1004@naver.com>
2023-10-06 10:55:43 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,14 +1,9 @@
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        for(char chr : my_string.toCharArray()) {
-            if(Character.isUpperCase(chr)) {
-                answer[chr - 'A']++;
-            } else if(Character.isLowerCase(chr)) {
-                answer[chr - 'a' + 26]++;
-            }
+        for (char ch : my_string.toCharArray()) {
+            answer[Character.isUpperCase(ch) ? (ch - 'A') : (ch - 'a' + 26)]++;
         }
-        
         return answer;
     }
 }
