2c6907f1a7a1e1cac58f9629b47d9835ebf7554e
송성빈 <aktm1004@naver.com>
2023-12-19 17:16:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.35 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,19 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         int[] answer = new int[score.length];
 
         for (int i = 0; i < score.length; i++) {
-            if (i < k) {
-                minHeap.add(score[i]);
-            } else {
-                if (score[i] > minHeap.peek()) {
-                    minHeap.poll(); // 최소값 제거
-                    minHeap.add(score[i]); // 새로값 추가
-                }
+            priorityQueue.add(score[i]); // 새로값 추가
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll(); // 최소값 제거
             }
-            answer[i] = minHeap.peek();
+            answer[i] = priorityQueue.peek();
         }
 
         return answer;
